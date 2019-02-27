4c24f6a2ce00fa01926eda89c263cb612bef7eb6
Bogdan Bulf <bogdan.bulf@gmail.com>
2019-02-27 12:57:10 +0200

    added files

--- src/Math.java
+++ src/Math.java
@@ -1,15 +1,18 @@
 
-
 public class Math {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        double[] arra={12,18,20};
-        System.out.println(stdev(arra));
-
-    }
-//bjsbisubfsbfjskfbks
+        int[] arr = {12, 18, 20};
+        System.out.println(sum(arr));
 
     }
 
-
+    public static int sum(int[] arr) {
+        int s = 0;
+        for (int i = 0; i < arr.length; i++) {
+            s = s + arr[i];
+        }
+        return s;
+    }
+}
